682bcc73ec0300b2c201e96ed31018b1e7843616
etiandre <eti.andre@gmail.com>
2024-05-16 17:03:41 +0200

    smooth NMF

--- nmf/beta_nmf.py
+++ nmf/beta_nmf.py
@@ -29,6 +29,9 @@ def beta_divergence(x: np.ndarray, y: np.ndarray, beta: float):
         )
     return np.mean(ret)
 
+def smooth_penalty(lambda_):
+    return lambda_ * (np.sum(R.flatten() - np.log(R.flatten())) - K * (N - 1))
+
 
 def mu_H(W, H, V, beta, l1_reg, l2_reg):
     Vhat = W @ H
@@ -43,6 +46,33 @@ def mu_H(W, H, V, beta, l1_reg, l2_reg):
 
     dH = num / dem
     return H * dH
+
+def mu_H_smooth(W, H, V, lambda_):
+    F, N = V.shape
+    
+    Vhat = W @ H
+    # Update H
+    dem = W.T @ (Vhat ** -1)
+    num = W.T @ (V * Vhat ** -2)
+    Ht = H.copy()
+
+    # first column: n = 0
+    p2 = dem[:, 0] + lambda_ / H[:, 1]
+    p1 = -lambda_
+    p0 = -num[:, 0] * Ht[:, 0]**2
+    H[:, 0] = (np.sqrt(p1**2 - 4 * p2 * p0) - p1) / (2 * p2)
+
+    # middle columns: n = 1 to N-2
+    for n in range(1, N - 1):
+        H[:, n] = np.sqrt((num[:, n] * Ht[:, n]**2 + lambda_ * H[:, n - 1]) / (dem[:, n] + lambda_ / H[:, n + 1]))
+
+    # last column: n = N-1
+    p2 = dem[:, N - 1]
+    p1 = lambda_
+    p0 = -(num[:, N - 1] * Ht[:, N - 1]**2 + lambda_ * H[:, N - 2])
+    H[:, N - 1] = (np.sqrt(p1**2 - 4 * p2 * p0) - p1) / (2 * p2)
+    
+    return H
 
 
 def mu_W(W, H, V, beta, l1_reg, l2_reg):
@@ -59,7 +89,6 @@ def mu_W(W, H, V, beta, l1_reg, l2_reg):
     dW = num / dem
     return W * dW
 
-
 class BetaNMF:
     def __init__(
         self,
@@ -69,10 +98,10 @@ class BetaNMF:
         beta: float,
         fixed_W: bool = False,
         fixed_H: bool = False,
-        algorithm="mu",
         alpha_W: float = 0,
         alpha_H: float = 0,
         l1_ratio: float = 0,
+        lambda_smooth: float = 0,
     ):
         assert not np.isnan(H.toarray()).any(), "H contains NaN"
         assert np.all(H.toarray() >= 0), "H is negative"
@@ -83,7 +112,13 @@ class BetaNMF:
         assert not np.isnan(V).any(), "V contains NaN"
         assert np.all(V >= 0), "V is negative"
         assert not (fixed_W and fixed_H), "Cannot fix both W and H"
-        assert algorithm in ["mu"]
+
+        if lambda_smooth > 0:
+            assert alpha_W == 0 and alpha_H == 0, "Cannot use l1 or l2 regularization with is_smooth"
+            assert beta == 0, "Cannot use beta!=0 with is_smooth"
+            algorithm = "is_smooth"
+        else:
+            algorithm = "mu"
 
         self.V = V  # FxN
         self.W = W  # FxK
@@ -91,6 +126,7 @@ class BetaNMF:
         self.beta = beta
         self.fixed_W = fixed_W
         self.fixed_H = fixed_H
+        self.lambda_smooth = lambda_smooth
         self.algorithm = algorithm
 
         # compute regularization
@@ -106,16 +142,15 @@ class BetaNMF:
                 self.H = mu_H(
                     self.W, self.H, self.V, self.beta, self.l1_reg_H, self.l2_reg_H
                 )
+            elif self.algorithm == "is_smooth":
+                self.H = mu_H_smooth(self.W, self.H, self.V, self.lambda_smooth)
             else:
                 raise NotImplementedError
 
         if not self.fixed_W:
-            if self.algorithm == "mu":
-                self.W = mu_W(
-                    self.W, self.H, self.V, self.beta, self.l1_reg_W, self.l2_reg_W
-                )
-            else:
-                raise NotImplementedError
+            self.W = mu_W(
+                self.W, self.H, self.V, self.beta, self.l1_reg_W, self.l2_reg_W
+            )
 
     def loss(self):
         loss = beta_divergence(self.V, self.W @ self.H, self.beta)
